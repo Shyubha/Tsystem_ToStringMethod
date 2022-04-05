75fd8c79e0deb638a3e0dd95f39c04e0044a781b
Shyubha <86038837+Shyubha@users.noreply.github.com>
2022-04-05 13:53:13 +0530

    Add project files.

---
+++ Employee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TSystems_ToStringMethode
+{
+    public class Employee
+    {
+        private int id;
+        private string name;
+        private string address;
+
+        public Employee(int id, string name, string address)
+        {
+            this.id = id;//this is used for parameter
+            this.name = name;
+            this.address = address;
+
+
+        }
+        public override string ToString()
+        {
+            return "empid " + id + "empname " + name + "emp address " + address;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TSystems_ToStringMethode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp = new Employee(1, "Amit", "Thane");
+            Console.WriteLine(emp);
+            Console.ReadLine();
+
+        }
+    }
+}
